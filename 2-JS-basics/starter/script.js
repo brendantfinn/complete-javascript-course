357c108ae1c11db7899cdc91fd098cc9d7c8c578
brendantfinn <brendantfinn@gmail.com>
2019-08-06 13:26:20 -0500

    Coding Challenge 2: Solved

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -222,54 +222,123 @@ GOOD LUCK 😀
 */
 
 
-var firstName = 'John';
-var age = 22;
+// var firstName = 'John';
+// var age = 22;
 
 
-//TERNARY OPERATOR
-age >= 18 ? console.log(firstName + ' drinks beer.')
-: console.log(firstName + ' drinks juice.');
+// //TERNARY OPERATOR
+// age >= 18 ? console.log(firstName + ' drinks beer.')
+// : console.log(firstName + ' drinks juice.');
 
-var drink = age >= 18 ? 'beer' : juice;
-console.log(drink);
+// var drink = age >= 18 ? 'beer' : juice;
+// console.log(drink);
 
-// if (age >= 18) {
-//     var drink = 'beer';
-// } else {
-//     var drink = 'juice';
+// // if (age >= 18) {
+// //     var drink = 'beer';
+// // } else {
+// //     var drink = 'juice';
+// // }
+
+// //SWITCH STATEMENT
+// var job = 'instructor';
+// switch (job) {
+//     case 'teacher':
+//     case 'instructor':
+//         console.log(firstName + ' teaches kids how to code.');
+//         break;
+//     case 'driver':
+//         console.log(firstName + ' drives an uber in Lisbon.');
+//         break;
+//     case 'designer': 
+//         console.log(firstName + ' designs beautiful websites.');
+//         break;
+//     default:
+//         console.log(firstName + ' does something else.')
 // }
 
-//SWITCH STATEMENT
-var job = 'instructor';
-switch (job) {
-    case 'teacher':
-    case 'instructor':
-        console.log(firstName + ' teaches kids how to code.');
-        break;
-    case 'driver':
-        console.log(firstName + ' drives an uber in Lisbon.');
-        break;
-    case 'designer': 
-        console.log(firstName + ' designs beautiful websites.');
-        break;
-    default:
-        console.log(firstName + ' does something else.')
+
+// age = 56;
+// switch(true) {
+//     case age < 13:
+//         console.log(firstName + ' is a boy.');
+//         break;
+//     case age >= 13 && age < 20:
+//         console.log(firstName + ' is a teenager.')
+//         break;
+//     case age>= 20 && age < 30:
+//         console.log(firstName + ' is a young man.')
+//         break;
+//     default:
+//         console.log(firstName + 'is a man.')
+
+//     }
+
+
+
+/*****************************
+* TRUTHY AND FALSY VALUES AND EQUALITY OPERATORS
+*/
+
+//falsey values: undefined, null, 0, '', NaN
+//Truthy values: NOT falsey Values
+
+
+// var height ;
+// height = '23';
+
+// if (height || height === 0) {
+//     console.log('Variable is defined');
+// } else {
+//     console.log('Variable has NOT been defined');
+// }
+
+// //Equality Operators
+// if (height == '23') {
+//     console.log('The == operator does type coercion!');
+// }
+
+
+
+/*****************************
+* CODING CHALLENGE 2
+*/
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+GOOD LUCK 😀
+*/
+
+var scoreJohn, scoreMike, scoreMary;
+scoreJohn = (89 + 120 + 103) / 3;
+scoreMike = (111 + 94 + 123) / 3;
+scoreMary = (97 + 134 + 105) / 3;
+// console.log(scoreJohn, scoreMike, scoreMary);
+
+/// SOLUTION TO STEPS 1-3
+// if (scoreJohn > scoreMike) {
+//     console.log('John\'s team wins with ' + scoreJohn + ' points');
+// } else if (scoreMike >scoreJohn) {
+//     console.log('Mike\'s team wins with ' + scoreMike + ' points');
+// } else {
+//     console.log('There is a draw')
+// }
+
+//SOLUTION TO EXTRA STEPS USING LOGICAL OPERATORS 
+if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+    console.log('John\'s team wins with ' + scoreJohn + ' points');
+} else if (scoreMike > scoreJohn & scoreMike > scoreMary) {
+    console.log('Mike\'s team wins with ' + scoreMike + ' points');
+} else if (scoreMary > scoreJohn & scoreMary > scoreMike) {
+    console.log('Mary\'s team wins with ' + scoreMike + ' points');
+} else {
+    console.log('There is a draw')
 }
-
-
-age = 56;
-switch(true) {
-    case age < 13:
-        console.log(firstName + ' is a boy.');
-        break;
-    case age >= 13 && age < 20:
-        console.log(firstName + ' is a teenager.')
-        break;
-    case age>= 20 && age < 30:
-        console.log(firstName + ' is a young man.')
-        break;
-    default:
-        console.log(firstName + 'is a man.')
-
-    }
 
