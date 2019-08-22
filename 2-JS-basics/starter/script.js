bebd263f30b4bd85d174ef81ad79ac76e5eb88b3
brendantfinn <brendantfinn@gmail.com>
2019-08-22 17:52:30 -0500

    CODING CHALLENGE 5 - completed (needed to watch solution)

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -556,42 +556,101 @@ GOOD LUCK 😀
 * LOOPS AND ITERATION
 */
 
-for (var i = 1; i < 20; i+= 2) {
-    console.log(i);
-}
+// for (var i = 1; i < 20; i+= 2) {
+//     console.log(i);
+// }
 
-// i = 0, 0 < 10 true, log i to console, i++
-// i =1, 1 < 10 true, log i to console, i++
-// ..happens a few more times
-// i = 9, 9 < 10 true, log i to console, i++
-// i =10, 10 < 10 FALSE, EXIT THE LOOP! 
+// // i = 0, 0 < 10 true, log i to console, i++
+// // i =1, 1 < 10 true, log i to console, i++
+// // ..happens a few more times
+// // i = 9, 9 < 10 true, log i to console, i++
+// // i =10, 10 < 10 FALSE, EXIT THE LOOP! 
 
 
-var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
-for (var i = 0; i < john.length; i++)
-console.log(john[i]);
+// var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+// for (var i = 0; i < john.length; i++)
+// console.log(john[i]);
 
-//While Loop
-var i = 0;
-while(i < john.length) {
-    console.log(john[i]);
-    i++;
-}
+// //While Loop
+// var i = 0;
+// while(i < john.length) {
+//     console.log(john[i]);
+//     i++;
+// }
 
-//continue and break statements
-var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
-for (var i = 0; i < john.length; i++){
-    if(typeof john[i] !== 'string') continue;
-    console.log(john[i]);
-}
+// //continue and break statements
+// var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+// for (var i = 0; i < john.length; i++){
+//     if(typeof john[i] !== 'string') continue;
+//     console.log(john[i]);
+// }
 
-for (var i =0; i<john.length; i++) {
-    if (typeof john[i] !== 'string') break;
-    console.log(john[i]);
-}
+// for (var i =0; i<john.length; i++) {
+//     if (typeof john[i] !== 'string') break;
+//     console.log(john[i]);
+// }
 
-//looping backwards
-for (var i = john.length -1; i >= 0; i--){
-console.log(john[i]);
-}
+// //looping backwards
+// for (var i = john.length -1; i >= 0; i--){
+// console.log(john[i]);
+// }
     
+
+
+
+/*****************************
+* CODING CHALLENGE 5
+*/
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+GOOD LUCK 😀
+*/
+
+
+var john = {
+    fullName: 'John Smith',
+    bills: [124, 48, 268, 180, 42],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for(var i = 0; i < this.bills.length; i++){
+            //Determine percentage based on tipping rules
+            var percentage;
+            var bill = this.bills[i];
+
+            if  (bill< 50){
+                percentage = .2;
+            } else if (bill >= 50 && bill < 200){
+                percentage = .15;
+            } else {
+                percentage = .1;
+            }
+            //add results to the corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+};
+john.calcTips();
+console.log(john);
